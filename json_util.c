5ef97107404435f5c26b26bb126c919bd0f33eed
Adam McKenzie <adam@entwinemedia.com>
2015-05-14 22:09:44 -0600

    CERV-635 Cleaning up json utility class

--- json_util.c
+++ json_util.c
@@ -4,11 +4,9 @@
 #include "json_util.h"
 
 /**
- * Gets a mandatory json string value.
+ * Gets a json string value.
  * @param parent
  *      The json object who is a parent to this key : string value pair.
- * @param resource_request
- *      The resource request that is retrieving the string value. Used to update the status in case something goes wrong.
  * @param json_key
  *      The string key that will identify the value to retrieve.
  * @return The string value if available, NULL if not.
@@ -17,23 +15,26 @@ char *get_json_string(json_t *parent, char *json_key) {
     json_t *obj = json_object_get(parent, json_key);
 
     if(!json_is_string(obj)) {
-        printf("Didn't find string '%s'\n", json_key);
         return NULL;
     } else {
         char *return_string = (char *)malloc(strlen(json_string_value(obj)) * sizeof(char) + 1);
         strcpy(return_string, json_string_value(obj));
-        printf("Found string %s\n", return_string);
         return return_string;
     }
 }
 
+/**
+ * Gets a json integer value.
+ * @param parent
+ *          The json object who is a parent to this key : integer value pair.
+ * @param json_key
+ *          The string that will identify the integer to retrieve.
+ */
 long long get_json_long_long(json_t *parent, char *json_key) {
     json_t *value = json_object_get(parent, json_key);
     if(!json_is_integer(value)) {
-        printf("The key was: '%s' value was: %s\n", json_key, json_string_value(value));
         return -1;
     } else {
-        printf("The new value: %lld\n", (long long)json_integer_value(value));
         return (long long)json_integer_value(value);
     }
 }
