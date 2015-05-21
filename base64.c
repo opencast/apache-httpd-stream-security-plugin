2ea46b6274cffb73751b25716036f133da48836f
Adam McKenzie <adam@entwinemedia.com>
2015-05-21 00:09:56 -0600

    SWITCHP-507 - Url signing plugin now adds padding to base 64 encoding if necessary

--- base64.c
+++ base64.c
@@ -23,6 +23,37 @@ size_t calc_decode_length(const char* b64input) { //Calculates the length of a d
 }
 
 /**
+ * Adds padding of "=" characters to end of Base 64 string if necessary.
+ * @param p
+ *      The pool to allocate memory to that will be cleaned upon end of session.
+ * @param b64message
+ *      The Base 64 encoded message.
+ */
+char *addPadding(apr_pool_t *p, char *b64message) {
+    size_t decodeLen = calc_decode_length(b64message);
+    size_t encodedLen = strlen(b64message);
+
+    if (decodeLen % 3 == 0) {
+        return b64message;
+    }
+
+    if (3 - (decodeLen % 3) == 1 && b64message[encodedLen - 1] == '=') {
+        char *withOnePadding = (char*) apr_palloc(p, sizeof(char) * (encodedLen + 2));
+        strncpy(withOnePadding, b64message, encodedLen);
+        withOnePadding[encodedLen] = '=';
+        withOnePadding[encodedLen + 1] = '\0';
+        return withOnePadding;
+    } else {
+        char *withTwoPadding = (char*) apr_palloc(p, sizeof(char) * (encodedLen + 3));
+        strncpy(withTwoPadding, b64message, encodedLen);
+        withTwoPadding[encodedLen] = '=';
+        withTwoPadding[encodedLen + 1] = '=';
+        withTwoPadding[encodedLen + 2] = '\0';
+        return withTwoPadding;
+    }
+}
+
+/**
  * Decodes a string into plain text.
  * @param p
  *          The pool to allocate memory to that will be cleaned upon end of session.
@@ -38,12 +69,14 @@ int base_64_decode(apr_pool_t *p, char* b64message, uint8_t** buffer, size_t* le
 	int decodeLen = calc_decode_length(b64message);
 	*buffer = (uint8_t*)apr_palloc(p, decodeLen);
 
-	bio = BIO_new_mem_buf(b64message, -1);
+    char *withPadding = addPadding(p, b64message);
+
+	bio = BIO_new_mem_buf(withPadding, -1);
 	b64 = BIO_new(BIO_f_base64());
 	bio = BIO_push(b64, bio);
 
 	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
-	*length = BIO_read(bio, *buffer, strlen(b64message));
+	*length = BIO_read(bio, *buffer, strlen(withPadding));
 	assert(*length == decodeLen); //length should equal decodeLen, else something went horribly wrong
 	BIO_free_all(bio);
 	return (0); //success
