8bfc77a288d9365d67b8e05f90284d9b9a9fbff9
Adam McKenzie <adam@entwinemedia.com>
2015-06-30 18:49:45 -0600

    CERV-792 Adding license headers to the source files

--- base64.c
+++ base64.c
@@ -1,4 +1,23 @@
-//Decodes Base64
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 #include <openssl/buffer.h>
@@ -64,7 +83,7 @@ char *addPadding(apr_pool_t *p, char *b64message) {
  * @param length
  *          The length of the plain text result.
  */
-int base_64_decode(apr_pool_t *p, char* b64message, uint8_t** buffer, size_t* length) { //Decodes a base64 encoded string
+int base_64_decode(apr_pool_t *p, char* b64message, uint8_t** buffer, size_t* length) {
 	BIO *bio, *b64;
 	int decodeLen = calc_decode_length(b64message);
 	*buffer = (uint8_t*)apr_palloc(p, decodeLen);
